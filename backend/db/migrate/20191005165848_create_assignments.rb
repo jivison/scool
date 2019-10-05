902adad0da2928011cbb6a8ebe2f073b7a033030
John Ivison <john@ivison.org>
2019-10-05 11:17:30 -0700

    Remove unecessary backend folder

--- backend/db/migrate/20191005165848_create_assignments.rb
+++
@@ -1,9 +0,0 @@
-class CreateAssignments < ActiveRecord::Migration[6.0]
-  def change
-    create_table :assignments do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
--- backend/db/migrate/20191005171054_add_assignment_references_to_course_assignments.rb
+++
@@ -1,5 +0,0 @@
-class AddAssignmentReferencesToCourseAssignments < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :course_assignments, :assignment, null: false, foreign_key: true
-  end
-end
