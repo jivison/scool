1c088c5efd970b4a7deccfe3b87fe3741456b50e
John Ivison <john@ivison.org>
2019-10-06 11:38:20 -0700

    Lay out framework for submissions

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,3 +1,12 @@
 class Submission < ApplicationRecord
   belongs_to :course_assignment
+
+  def submitter
+    User.find(self.course_role_submitter_id)
+  end
+
+  def marker
+    User.find(self.course_role_marker_id)
+  end
+
 end
