94d2d3f181581e21779c59abe126ebf4961c7463
Derick Del Rosario <derickmitchel@icloud.com>
2019-10-06 16:58:26 -0700

    create navbar2 styles

--- config/routes.rb
+++ config/routes.rb
@@ -12,15 +12,22 @@ Rails.application.routes.draw do
   get 'home/index'
 
   resources :assignments
-  
+
   resources :courses do
     resources :course_assignments do
-      resources :submissions
+      resources :submissions do
+        get "edit_grade", as: :edit_grade
+        patch "update_grade", as: :update_grade
+      end
     end 
   end
 
   resources :assignments
-  
+
   resources :sessions, only: [:create, :destroy, :new]
 
+  get "login", to: "sessions#new"
+  get "sign_in", to: "sessions#new"
+
 end
+
