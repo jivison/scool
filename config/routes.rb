61468fe7c56c1b9fb847c303c45eb1deb7d68149
John Gootee <gootee1565@gmail.com>
2019-10-06 21:22:58 -0700

    create archive courses report started

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
   get 'courses/show'
-  # get 'assignments/index'
-  # get 'assignments/new'
-  # get 'assignments/create'
-  # get 'assignments/edit'
-  # get 'assignments/update'
-  # get 'assignments/delete'
-  # get 'assignments/destroy'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'courses/archive'
 
+  get 'users/courses'
+  
   root to: "home#index"
   get 'home/index'
 
