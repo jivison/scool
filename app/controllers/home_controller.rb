04ecd62e45f206d87c10a39e5f668a7234b0ddab
Derick Del Rosario <derickmitchel@icloud.com>
2019-10-06 21:59:42 -0700

    asfas

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,7 @@
 class HomeController < ApplicationController
     def index
-<<<<<<< HEAD
-        @courses = [Course.last]
-        @assignments = CourseAssignment.where(course_id: @courses[0].id)
-        @date =
-=======
         @courses = current_user.course_roles.map(&:course)
         @assignments = @courses.map(&:course_assignments).flatten
->>>>>>> 3f057a952a23f14b3f426c5b2d4afe5894d19902
 
         # role = current_user.current_role.type
         role = "admin"
